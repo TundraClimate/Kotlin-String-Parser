b6bbb206ea24f10af7e29973fe45f064ab4ac2d9
TundraClimate <tundra.tundere@gmail.com>
2022-10-08 20:21:43 +0900

    refactor: change logic

--- src/main/kotlin/io/github/tundraclimate/StringParser.kt
+++ src/main/kotlin/io/github/tundraclimate/StringParser.kt
@@ -26,20 +26,22 @@ class StringParser private constructor(private var parsingText: String) {
         charStack += stack
     }
 
+    fun getCharStack() = charStack
+
     //Parsing String
-    private fun parsing(each: (charStack: String, lastChar: Char) -> String): String {
+    private fun parsing(each: (lastChar: Char) -> String): String {
         var result = ""
         parsingText.forEach {
             lastChar = it
-            val s = each(charStack, lastChar)
+            val s = each(lastChar)
             result += s
         }
         return result
     }
 
     //Wrapped Parser
-    fun parse(override: String = "", parsing: StringParser.(lastChar: Char, charStack: String) -> String): String {
+    fun parse(override: String = "", parsing: StringParser.(lastChar: Char) -> String): String {
         if (override.isNotEmpty()) parsingText = override
-        return parsing { cs, lc -> this.parsing(lc, cs) }
+        return parsing { lc -> this.parsing(lc) }
     }
 }

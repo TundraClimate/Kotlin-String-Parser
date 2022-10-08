59fb814d493df7b9c7f05d1e38e7c480d200e992
TundraClimate <tundra.tundere@gmail.com>
2022-10-08 20:31:28 +0900

    test: add TestCode

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -7,7 +7,7 @@ class Test {
     fun testParse() {
         //example code
         val parser = StringParser.from("{ me: \"you\" }")
-        val parsed = parser.parse { lastChar, _ ->
+        val parsed = parser.parse { lastChar ->
             when (lastChar) {
                 ' ' -> ""
                 '[' -> "{"
@@ -17,5 +17,32 @@ class Test {
             }
         }
         assertEquals(parsed, "{me:\\\"you\\\"}")
+    }
+
+    @Test
+    fun testParse2() {
+        //second example code
+        val parser = StringParser.from("\"{<--String-->}\" -- {<--Raw-->}")
+        var isStr = false
+        val parsed = parser.parse { lastChar ->
+            if (isStr) {
+                addCharStack(lastChar.toString())
+                if (lastChar == '\"') {
+                    isStr = false
+                    getCharStack()
+                } else ""
+            } else when (lastChar) {
+                '{' -> "-:"
+                '}' -> ":-"
+                '\"' -> {
+                    isStr = true
+                    addCharStack(lastChar.toString())
+                    ""
+                }
+
+                else -> lastChar.toString()
+            }
+        }
+        println(parsed)
     }
 }
